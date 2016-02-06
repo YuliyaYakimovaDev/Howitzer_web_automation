84c5ab0076301685b854dd66e8777966c8814a6d
YuliyaYakimovaDev <yakimovayuliya@i.ua>
2016-02-06 19:43:31 +0200

    initialize repository

--- test_automation_rspec/emails/confirmation_instruction_mail.rb
+++ test_automation_rspec/emails/confirmation_instruction_mail.rb
@@ -2,15 +2,17 @@ class ConfirmationInstructionEmail < Email
   SUBJECT = "Your account registration with nCrypted Cloud"
 
   def addressed_to?(new_user)
-    /Hi #{new_user},/ === plain_text_body
+    /Hi #{new_user}/ === plain_text_body
   end
 
   def confirm_my_account
+    log.info "confirm my acount"
     WebPage.visit confirmation_link
   end
 
   def confirmation_link
-    res = plain_text_body.scan(/"(http(s?):\/\/.+)\/\W/)
+    log.info "confirmation link"
+    res = plain_text_body[/href="(http(s?):\/\/.+)\/" target="_blank"\>Continue</,1]
     log.error("Confirmation link was not found") if res.nil?
     res
   end
